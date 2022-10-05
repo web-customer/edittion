053aaf5a61f4a46b2524ec24fe6fd6cd6ed28224
tqhoa <tqhoa8th@gmail.com>
2022-10-05 10:14:46 +0700

    tete

--- gallery.php
+++ gallery.php
@@ -1,61 +1,87 @@
 <?php
 include "core.php";
-head();
+include "header.php";
 ?>
-	<div class="col-md-12">
-        <div class="card">
-            <div class="card-header"><i class="fa fa-images"></i> Gallery</div>
-                <div class="card-body">
+
+
+    <!-- Breadcrumb -->
+    <div class="bn-breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <ol class="bn-breadcrumb">
+                        <li>
+                            <i class="fa fa-home"></i>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li><i class="fa fa-angle-right"></i>Gallery</li>
+                    </ol>
+                </div>
+                <!-- Col End -->
+            </div>
+            <!-- Row End -->
+        </div>
+        <!-- Container End -->
+    </div>
+    <!-- Breadcrumb End -->
+    <!-- Start Main Content -->
+    <section class="main-content bn-category-grid pt-0">
+        <div class="container">
+            <div class="row bn-gutter-30">
+                <div class="col-lg-8 col-md-12">
                     <div class="row">
-<?php
-$run   = mysqli_query($connect, "SELECT * FROM `gallery` WHERE active='Yes' ORDER BY id DESC");
-$count = mysqli_num_rows($run);
-if ($count <= 0) {
-    echo '<div class="alert alert-info">There are no added images.</div>';
-} else {
-    while ($row = mysqli_fetch_assoc($run)) {
-        echo '
-
-            <div class="col-md-4 mb-3" data-bs-toggle="modal" data-bs-target="#p' . $row['id'] . '" class="col-md-4">
-             
-                <div class="card shadow-sm">
-                    <img src="' . $row['image'] . '" alt="' . $row['title'] . '" style="width: 100%; height: 180px;">
-
-                    <div class="card-body">
-                        <h6 class="card-title">' . $row['title'] . '</h6>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <button type="button" data-bs-toggle="modal" data-bs-target="#p' . $row['id'] . '" class="btn btn-sm btn-outline-secondary col-12">
-                                <i class="fas fa-info-circle"></i> Details
-                            </button>
+                        <div class="col-md-6">
+                            <div class="bn-post-block-style">
+                                <div class="bn-post-thumb">
+                                    <a href="#">
+                                        <img class="img-fluid" src="assets/images/800x520.png" alt="">
+                                    </a>
+                                    <div class="bn-category">
+                                        <a class="bn-post-category" href="#">Tech</a>
+                                    </div>
+                                </div>
+                                <!-- Post Thumb End -->
+                                <div class="bn-post-content">
+                                    <h2 class="bn-post-title title-md">
+                                        <a href="#">It wasn’t raining when Noah built the ark</a>
+                                    </h2>
+                                    <div class="bn-post-meta bn-mb-7">
+                                        <span class="bn-post-author"><a href="#"><i class="fa fa-user"></i> James Bond</a></span>
+                                        <span class="bn-post-date"><i class="far fa-clock"></i> 26 Jan, 2021</span>
+                                    </div>
+                                    <p>True friendship is perhaps the only relation that survives the trials and tribulations of time and remains unconditional.</p>
+                                </div>
+                                <!-- Post Content End -->
+                            </div>
+                            <!-- Post Block Style End -->
                         </div>
                     </div>
+                    <!-- Row End -->
+                    <div class="bn-gap-30"></div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="load-more-btn text-center">
+                                <button class="btn"> Load More </button>
+                            </div>
+                        </div>
+                        <!-- Col End -->
+                    </div>
+                    <!-- Row End -->
                 </div>
-            
-            </div>
-
-			<div class="modal" id="p' . $row['id'] . '" tabindex="-1" role="dialog">
-			  <div class="modal-dialog modal-lg">
- 			   <div class="modal-content">
- 			     <div class="modal-header">
- 			       <h5 class="modal-title">' . $row['title'] . '</h5>
-                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
- 			     </div>
-			     <div class="modal-body">
-				      <img src="' . $row['image'] . '" width="100%" height="auto" alt="' . $row['title'] . '" /><br /><br />
-				      ' . html_entity_decode($row['description']) . '
-				 </div>
-			    </div>
-			  </div>
-			</div>
-';
-    }
-}
-?>
+                <!-- Col End -->
+                <div class="col-lg-4 col-md-12">
+                    <!-- Start Sidebar -->
+                    <?php include "sidebar.php"; ?>
+                    <!-- Sidebar End -->
                 </div>
+                <!-- Col End -->
             </div>
+            <!-- Row End -->
         </div>
-	</div>
-</div></div>
+        <!-- Container End -->
+    </section>
+    <!-- Main Content End -->
+
 <?php
-footer();
+include "footer.php";
 ?>
